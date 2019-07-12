8f3a703fa3b8894029faded1f7dd3eeef0204f31
Jimil Patel <jimil.patel@hulu.com>
2019-07-11 19:09:42 -0700

    created a v2, with a defined folder structure. client folder holds the classifier and the source code. also copied an accurate eye-tracker from github: https://github.com/stepacool/Eye-Tracker

---
+++ v2/client/main.py
@@ -0,0 +1,28 @@
+"""
+main.py
+
+Capture data from a webcam,
+process the data using detect.py
+Take appropriate actions with that data.
+"""
+
+import cv2
+import detect
+
+class ClientWrapper():
+    """Client wrapper."""
+
+    def __init__(self):
+        self.webcam_running = False
+
+    def start_webcam(self):
+
+
+
+
+if __name__ == "__main__":
+    client = ClientWrapper()
+
+
+
+
